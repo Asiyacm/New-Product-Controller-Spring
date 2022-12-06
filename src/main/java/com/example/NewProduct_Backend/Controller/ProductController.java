fa98625e445c2044173cc8c31e33e37a42480663
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-06 16:07:18 +0530

    retrive commit

--- src/main/java/com/example/NewProduct_Backend/Controller/ProductController.java
+++ src/main/java/com/example/NewProduct_Backend/Controller/ProductController.java
@@ -3,31 +3,31 @@ package com.example.NewProduct_Backend.Controller;
 import com.example.NewProduct_Backend.Model.Product;
 import com.example.NewProduct_Backend.dao.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
 
     @Autowired
     private ProductDao dao;
+    @CrossOrigin(origins = "*")
    @GetMapping("/")
     public String Homepage(){
         return "Welcome to Home page";
     }
-
+    @CrossOrigin(origins = "*")
     @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
     public String Addpage(@RequestBody Product p){
         System.out.println(p.getPname().toString());
         dao.save(p);
         return "Welcome to Product Add page";
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public String Viewpage(){
-        return "Welcome to Product View page";
+    public List<Product> Viewpage(){
+        return(List<Product>)dao.findAll();
     }
 }
 
