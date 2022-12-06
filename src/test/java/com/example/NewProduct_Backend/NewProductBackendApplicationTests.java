739f1af66d36829acc6a192a0777a2055d4f42ed
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-06 15:34:07 +0530

    initial commit

---
+++ src/test/java/com/example/NewProduct_Backend/NewProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewProduct_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProductBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
